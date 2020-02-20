d9a9d9c0d5ec62ba346aba4f15031d22d507fd6c
root <root@ip-10-1-1-179.ec2.internal>
2020-02-20 11:56:31 +0000

    Added another function

--- script.sh
+++ script.sh
@@ -3,6 +3,9 @@
 func(){
 echo "This is test function for $i"
 }
+func2(){
+echo "This is second function"
+}
 for i in {1..100}
 do
 if [ $(expr $i % 2) -eq 0 ]
@@ -12,6 +15,7 @@ else
 echo "$i is Odd Number"
 fi
 func
+func2
 done
 
 
